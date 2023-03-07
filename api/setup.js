1470e3221dbf42a9be6e1d64f4bc1d67c77cd20e
Shabin k <73272797+SHABIN-K@users.noreply.github.com>
2023-03-07 11:05:41 +0530

    Update setup.js

--- api/setup.js
+++ api/setup.js
@@ -1,19 +1,10 @@
 module.exports = function(request, response) {
-    /**
-     * Intégration des dépendences SQLITE3
-     */
+
     const sqlite3 = require('sqlite3').verbose();
     const db = new sqlite3.Database('./db/data.db');
-
-    /**
-     * Dépendance permettant la modification du fichier config et la génération du mot de passe de l'api
-     */
     const fs = require('fs');
     const generator = require('generate-password');
 
-    /**
-     * Création de la DB ainsi que les tables contenues dedans
-     */
     db.serialize(function() {
         db.run('CREATE TABLE IF NOT EXISTS calls (itsfrom TEXT, itsto TEXT, digits TEXT, callSid TEXT, status TEXT, date TEXT, user TEXT, name TEXT, service TEXT)');
         db.run('CREATE TABLE IF NOT EXISTS sms (itsfrom TEXT, itsto TEXT, smssid TEXT, content TEXT, status TEXT, date TEXT, user TEXT, service TEXT)');
