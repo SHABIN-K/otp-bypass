d32b0a8d4b3d98bc09e4acf1e68d59e651d2b6ed
Shabin k <73272797+SHABIN-K@users.noreply.github.com>
2023-03-07 11:07:01 +0530

    Update api.js

--- api/api.js
+++ api/api.js
@@ -2,9 +2,7 @@ var app = require('./app');
 var config = require('./config');
 var http = require('http');
 
-/**
- * Côté serveur web HTTP lançant la partie EXPRESS
- */
+
 var server = http.createServer(app);
 server.listen(config.port || 80, function() {
     console.log('Express server started on *:' + config.port);
